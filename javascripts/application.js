1717251107bec7c64dffe6d059cbd72dcb608ee2
Xavier Bourguignon <xavierbourguignon@gmail.com>
2010-12-12 00:00:12 +0100

    Improving speaker pages, changing sponsor page, sponsors content in textile

---
+++ javascripts/application.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    $("a.load").click(function() {
+        $("#details").hide();
+        if ($("#details").attr('memo_url') == this.href) {
+            $("#details").attr('memo_url', '');
+        } else {
+            $("#details").attr('memo_url', this.href);
+            $.get(this.href,
+                    function (c) {
+                        $("#details")
+                                .html(c)
+                                .fadeIn('slow');
+                    }
+                    );
+        }
+        return false;
+    });
+});
