4c1426531bfc7b92781223c5bf549d73f07291e1
Xavier Bourguignon <xavierbourguignon@gmail.com>
2010-12-14 22:38:07 +0100

    impoving sponsor page, adding Altran, switc to markdown

--- javascripts/application.js
+++ javascripts/application.js
@@ -11,7 +11,24 @@ $(document).ready(function () {
                                 .html(c)
                                 .fadeIn('slow');
                     }
-                    );
+            );
+        }
+        return false;
+    });
+
+    $("a.load-premium").click(function() {
+        $("#details-premium").hide();
+        if ($("#details-premium").attr('memo_url') == this.href) {
+            $("#details-premium").attr('memo_url', '');
+        } else {
+            $("#details-premium").attr('memo_url', this.href);
+            $.get(this.href,
+                    function (c) {
+                        $("#details-premium")
+                                .html(c)
+                                .fadeIn('slow');
+                    }
+            );
         }
         return false;
     });
